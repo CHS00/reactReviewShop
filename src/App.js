b5d7d82f4e548f6843949cb2ed2dee05dee6e196
CHS00 <claw0605@gmail.com>
2023-01-31 09:17:48 +0900

    ajax

--- src/App.js
+++ src/App.js
@@ -13,16 +13,22 @@ import data from "./data"
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import Detail from './routes/Detail';
 
+import axios from "axios"
+import Loading from "./loading/Loading"
+
 
 
 function App() {
 
-  let [shoes] = useState(data);
+  let [shoes,setShoes] = useState(data);
 
   // 아래와 같이 use어쩌구 하고 시작하는것을 hook이라 한다.
   // hook이란 유용한 내용들이 들어있는 함수를 말한다.
   // useNavigate는 페이지 이동을 도와주는 함수
   let navigate =  useNavigate();
+
+  let [count,setCount] = useState(1);
+  let [loading,setLoading] = useState(false);
 
   return (
     <div className="App">
@@ -30,7 +36,7 @@ function App() {
       {/* {변수}, {b} 와 같이 export default된 변수를 가져올 수 있다. */}
       <Navbar bg="white" variant="white">
         <Container>
-          <Navbar.Brand href="#home">쇼핑몰</Navbar.Brand>
+          <Navbar.Brand href="/">쇼핑몰</Navbar.Brand>
           <Nav className="me-auto">
             <Link to="/" className='page-name'>홈</Link> 
             {/* useNavigate 사용법 */}
@@ -40,7 +46,10 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-
+      
+      {
+        loading?<Loading/>:null //로딩화면
+      }
 
       {/* 라우터를 쓸때는 index.js에서 BrowserRouter 추가 */}
       <Routes>
@@ -58,8 +67,67 @@ function App() {
                 )
               })
             }
+            
+            {
+              count!==3
+              ?<button onClick={()=>{
+
+                setLoading(true)
+                // ajax를 사용하는 3가지 방법
+                // 1. XMLHttpRequest (old)
+                // 2.fetch() (new)
+                // 3.axios (라이브러리)
+                
+                // axios를 통한 get요청은 axios를 import한 뒤,
+                // 아래와 같이 axios.get("url")
+                axios.get("https://codingapple1.github.io/shop/data"+Number(count+1)+".json")
+                // 가져온 데이터를 출력할 때는 .then((파라미터)=>{})
+                // 파라미터가 가져온 데이터
+                .then((result)=>{
+                  setLoading(false)
+
+                  console.log(result);
+                  let updatedShoes = [...shoes,...result.data];
+                  
+                  setShoes(updatedShoes);
+                  setCount(count+=1)
+                })
+                // ajax요청을 실패할 경우에 실행할 코드는 catch(()=>{})
+                .catch(()=>{
+                  setLoading(false)
+                  console.log("실패했서요")
+                });
+
+                // 서버로 데이터를 전송할 때 사용하는 POST요청
+                // axios.post("/url",{name:"보낼 데이터"});
+
+                // ajax요청을 동시에 여러개 하기위해선,
+                // ex) axios.get("/url1"); axios.get("/url2");
+                // Promise.all([axios.get("/url1"),axios.get("/url2")])
+                // .then(()=>{});
+                // 두 요청을 모두 성공했을때 아래의 코드를 실행시키게 함
+
+                // 원래 서버와 데이터를 주고받을때는 문자만 주고받을수 있음.
+                // 따라서 배열이나 객체 데이터는 주고받을 수 없으나,
+                // "{"name":"kim"}" 과 같이 따옴표를 다 쳐두면
+                // 문자로 인식하여, 가져올 수 있게 됨.(이를 JSON데이터라 함)
+
+                // 아래는 fetch로, axios없이도 get요청 가능한데,
+                // fetch("https://codingapple1.github.io/shop/data2.json")
+                // fetch에서 객체등을 가져오기 위해선 아래의 것을 써주어야함
+                // 아래의 코드로 json을 array/object로 변환시켜준다.
+                // .then(결과=>결과.json())
+                // .then(data=>{})
+                // 위와같은 번거로움을 없애기 위하여 axios를 사용한다.
+              }}>버튼</button>
+              :null
+            }
+
           </div>
+          
           }/>
+
+          
 
         {/* :id는 /detail/아무거나 라는 뜻 (URL파라미터라고 함) */}
         <Route path='/detail/:id' element={<Detail shoes={shoes}/>}/>
@@ -92,8 +160,12 @@ function App() {
 
 function Prod(props){
 
+  let navigate =  useNavigate();
+
   return(
-      <div className='col-md-4'>
+      <div className='col-md-4' onClick={()=>{
+        navigate(`/detail/${props.index-1}`)
+      }}>
         <img 
         src={"https://codingapple1.github.io/shop/shoes"+props.index+".jpg"} 
         width="80%"/>
@@ -120,6 +192,20 @@ function Event(){
     </div>
   )
 }
+
+//서버에 데이터를 요청하는 법
+// 1.방법 (get/post)
+// 2.어떤자료 (url형식으로 기입)
+
+// 예를 들어 웹툰의 데이터를 가져오고싶다면
+// comic.naver.com으로 get요청을 한다.
+// 네이버 서버에 글을 올리고싶다면 
+// blog.naver.com으로 post요청을 하여 서버로 데이터를 전달한다.
+
+// 누군가 해당url을 get요청하면 웹툰을 가져다 주게 코드를 짜게 된다.
+
+// 원래는 get/post요청을 할 시, 새로고침이 실행되나,
+// ajax를 사용하여 요청할 시, 새로고침이 실행되지않는다.
 
 
 export default App;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,28 +1,151 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components"
+import {Nav} from 'react-bootstrap';
+
+
+// 위와 같이 styled components를 사용하면,
+// 해당 스타일이 다른 js로 오염되지 않는다.
+// 또한 페이지 로딩시간도 단축시켜준다.
+let YellowBtn = styled.button`
+    background : ${props => props.bg };
+    color : ${props => props.bg=="blue"?"white":"black"};
+    padding : 10px;
+`
+// YellowBtn을 사용할때 bg라는 props를 사용할 수 있다고 선언하는 것
+// (그냥 라이브러리 사용법이므로 외울 필요는 없음.)
+
+
+// Lifecycle
+// 페이지가 보이는 순간 = mount
+// 페이지가 업데이트 되는 것 = update
+// 페이지를 이동하여 컴포넌트가 보이지않게 되며 사라지는것 = unmount
 
 function Detail(props){
+
+    let [count,setCount] = useState(0)
+    let [alert2,setAlert2] = useState(true)
+
+    let [num,setNum] = useState("")
+
+    let [tab,setTab] = useState(0)
+
+    useEffect(()=>{
+        let a = setTimeout(() => {
+            setAlert2(false);
+        }, 2000);
+        // 아래와 같이 return을 넣으면
+        // useEffect가 실행되기전에 내부의 코드가 실행된다.
+        console.log(2)
+
+        if (isNaN(num)) {
+            alert("숫자만 쓰세요")
+        }
+        return ()=>{
+            console.log(1)
+            // 보통 기존의 코드를 제거 해둘때 자주 사용한다.(clean up fn)
+            // 아래와 같이 타이머를 삭제해둘수 있다.
+            // (clean up function은 mount시에는 실행되지않고,
+            // unmount시에 실행된다.)
+            clearTimeout(a);
+            // 다른 예로는 서버로 데이터를 요청하는 코드가 있다 했을 때,
+            // 데이터를 가져오는 도중에 몇번씩 재랜더링이 된다면
+            // 수개의 데이터 요청이 발생하므로
+            // 기존 데이터 요청은 제거해주는 코드를 입력할 수 있을 것이다.
+        }
+        // []내부에 useEffect의 실행조건을 넣을 수 있다.
+        // 아래와 같이 작성 시, count가 변할 때만 실행된다.
+        // []를 비워놓으면 mount, update가 아니라 mount시에만 실행된다.
+    },[count,num])
+    // useEffect는 mount, update시 코드를 실행해주는 기능
+    // 내부의 코드는 html이 모두 랜더링 된 뒤에 실행된다.
     
-    // useParams은 현재 url의 파라미터정보들을 가져온다
+    // 예를 들어 내부에 위와같이 코드를 입력하면 아래의
+    // console.log("안녕")이 먼저 실행되고 나서,
+    // useEffect 내부의 코드가 실행된다.
+
+    // 보통은 어려운 연산이나 서버에서 데이터를 가져오는 작업,
+    // 타이머 장착 등에 사용한다.
+    console.log("안녕")
+
+    // useEffect를 정리하자면,
+    // useEffect(()=>{  }) 1.재랜더링마다 코드를 실행하고 싶은 경우.
+    // useEffect(()=>{  },[]) 2.mount시 1회 코드 실행하고 싶은 경우.
+    // useEffect(()=>{
+    //     return()=>{ xx }
+    // }) 3.unmount시 1회 코드 실행하고 싶은 경우.
+    // 4.useEffect실행 전에 뭔가 실행하려면 언제나 return()=>{}
+    // 5.특정 state변경 시에만 실행하려면 [state명]
+    
+
+
+    // useParams은 현재 url의 파라미터정보들을 가져온다(use~~이므로 훅임)
     // 유저가 :id 자리에 적은것을 가져와 줌
     let {id} = useParams();
     let nowshoes = props.shoes.find((value)=>value.id==id);
-    console.log(nowshoes)
 
     return(
         <div className="container">
+            {alert2?
+            <div className="alert alert-warning">
+                2초 이내 구매시 할인
+            </div>:
+            null}
+            
+
+            {count}
+            <button onClick={()=>{setCount(count+1)}}>useEffect를 알아보기위한 버튼</button>
+
+            <YellowBtn bg="blue">버튼</YellowBtn>
+            <YellowBtn bg="yellow">버튼</YellowBtn>
+            <input type="text" className="input" onChange={
+                (e)=>{setNum(e.target.value)}
+            }/>
+
             <div className="row">
-            <div className="col-md-6">
-                <img src={"https://codingapple1.github.io/shop/shoes"+Number(nowshoes.id+1)+".jpg"} width="100%" />
+                <div className="col-md-6">
+                    <img src={"https://codingapple1.github.io/shop/shoes"+Number(nowshoes.id+1)+".jpg"} width="100%" />
+                </div>
+
+
+                <div className="col-md-6">
+                    <h4 className="pt-5">{nowshoes.title}</h4>
+                    <p>{nowshoes.content}</p>
+                    <p>{nowshoes.price}</p>
+                    <button className="btn btn-danger">주문하기</button> 
+                </div>
             </div>
-            <div className="col-md-6">
-                <h4 className="pt-5">{nowshoes.title}</h4>
-                <p>{nowshoes.content}</p>
-                <p>{nowshoes.price}</p>
-                <button className="btn btn-danger">주문하기</button> 
-            </div>
-            </div>
+
+            <Nav fill variant="tabs" defaultActiveKey="link-0">
+                <Nav.Item>
+                    <Nav.Link eventKey="link-0" onClick={()=>{setTab(0)}}>1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-1" onClick={()=>{setTab(1)}}>2</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-2" onClick={()=>{setTab(2)}}>3</Nav.Link>
+                </Nav.Item>
+            </Nav>
+
+            <Tab tab={tab}/>
+
         </div> 
     )
+}
+
+function Tab({tab}) {
+    // 위처럼 중괄호안에 props를 전달하면 아래에 props를 입력하지않아도 된다.
+    return(
+        [<div>내용1</div>,<div>내용2</div>,<div>내용3</div>][tab]
+    )
+    // if (tab == 0) {
+    //     return (<div>내용1</div>)
+    // }else if (tab == 1) {
+    //     return (<div>내용2</div>)
+    // }else if (tab == 2) {
+    //     return (<div>내용3</div>)
+    // }
 }
 
 export default Detail;
